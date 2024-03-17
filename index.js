2098daec40e2ca872d58969bbf320e082b4366ef
Shiban Ashiq <shibanashiq743@gmail.com>
2024-03-17 13:07:11 +0530

    commt

---
+++ index.js
@@ -0,0 +1,41 @@
+import express from "express";
+import bodyParser from "body-parser";
+import request from "request";
+const app = express();
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());;
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.post("/url/request", (req, res) => {
+    const { url, proxy } = req.body;
+
+    try {
+        var options = {
+            'method': 'GET',
+            'url': url,
+            'proxy': proxy,
+            'headers': {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
+                'Content-Type': 'application/json',
+            },
+        };
+
+        request(options, function (error, response) {
+            if (error) res.send(error); else res.send(response?.body);
+        });
+
+    } catch (error) {
+        res.send(error);
+    }
+
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
